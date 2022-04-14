6a97a014c825eff1c47ec6ea88711e9b02acf3fa
SUAT ARISOY <cevriyeceven@gmail.com>
2022-04-14 18:34:25 +0200

    first commit

---
+++ movie-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg'
+import './App.css'
+import AuthContextProvider from './context/AuthContext'
+import AppRouter from './router/Router'
+
+function App() {
+  return (
+    <AuthContextProvider>
+      <AppRouter />
+    </AuthContextProvider>
+  )
+}
+
+export default App
